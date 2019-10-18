de187c0a69d75d543c00d3754ec85c4e10eda53e
unknown <ste.daff@gmail.com>
2019-10-18 15:25:53 -0700

    created stamper brush

--- Assets/Scripts/Draw.cs
+++ Assets/Scripts/Draw.cs
@@ -7,6 +7,8 @@ public class Draw : MonoBehaviour
 
     public Material lineMat;
     public GameObject hand;
+    public float lineWidth = 0.1f;
+    public GameObject drawingParent;
     private LineRenderer line;
     private MeshLineRenderer currLine;
     private int numClicks = 0;
@@ -15,26 +17,28 @@ public class Draw : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        currLine = new MeshLineRenderer();
     }
 
     // Update is called once per frame
     void Update()
     {
+
         float triggerVal = OVRInput.Get(OVRInput.Axis1D.PrimaryIndexTrigger);
-        if (triggerVal < 0.3) 
+        if (triggerVal < 0.5 && triggerVal > 0.3) 
         {
             GameObject go = new GameObject();
             go.AddComponent<MeshFilter>();
             go.AddComponent<MeshRenderer>();
+            go.transform.SetParent(drawingParent.transform);
             currLine = go.AddComponent<MeshLineRenderer>();
-
+            //currLine.gameObject.AddComponent<Water>();
             currLine.lmat = new Material(lineMat);
-            currLine.setWidth(0.1f);
+            currLine.setWidth(lineWidth);
 
             numClicks = 0;
         }
-        else
+        else if (triggerVal > 0.8)
         {
             currLine.AddPoint(hand.transform.position);
             numClicks++;
---
+++ Assets/Scripts/Stamp.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Stamp : MonoBehaviour
+{
+
+    public Sprite spriteImage;
+    public Sprite[] sprites;
+    public GameObject drawingParent;
+    public GameObject hand;
+    public float size;
+    public float spread;
+
+    private SpriteRenderer sprite;
+    private int numClicks = 0;
+    private GameObject currStamp;
+    private Quaternion stampRotation;
+    private int listSize;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        listSize = sprites.Length;
+        currStamp = new GameObject();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float triggerVal = OVRInput.Get(OVRInput.Axis1D.PrimaryIndexTrigger);
+
+        if (triggerVal < 0.5 && triggerVal > 0.3)
+        {
+            currStamp = new GameObject();
+            currStamp.transform.SetParent(drawingParent.transform);
+            stampRotation = hand.transform.rotation;
+            numClicks = 0;
+        }
+
+        else if (triggerVal > 0.8)
+        {
+            GameObject go = new GameObject();
+            go.transform.position = hand.transform.position;
+            go.transform.localScale = new Vector3(size, size, size);
+            sprite = go.AddComponent<SpriteRenderer>();
+            go.transform.SetParent(currStamp.transform);
+            sprite.sprite = sprites[numClicks % listSize];
+            numClicks++;
+        }
+
+    }
+}
